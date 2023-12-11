b0f68555bac609ce83469a9769a8a6ff96434d30
楊竣宇 <yangjunyu@yangjunyudeAir.hitronhub.home>
2023-12-11 19:36:27 +0800

    優化字體和圖像

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Noto_Sans_SC } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
+export const notoSansTC = Noto_Sans_SC({ subsets: ['latin-ext'] });
