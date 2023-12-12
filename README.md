0080d6ca07eff75b3d00d1fc918753a1847b6b44
楊竣宇 <yangjunyu@yangjunyudeAir.hitronhub.home>
2023-12-12 21:45:25 +0800

    ServerComponent的應用

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This is the starter template for the Next.js App Router Course. It contains the 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
 
 ## Current Progress
-https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#why-optimize-images
+https://nextjs.org/learn/dashboard-app/fetching-data
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,49 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
+
+/*
+Server Components 無法使用 props、state、event listeners 
+(ex: onClick(), onChange() 等等 ) 
+或像是 useState、useEffect、useReducer 等 React hooks。
+
+Server Component 要注意：
+1.資料請求正在無意中創建瀑布。
+2.儀表板是靜態的，因此任何資料更新都不會反映在您的應用程式上。
+*/
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  const latestInvoices = await fetchLatestInvoices();
+
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
