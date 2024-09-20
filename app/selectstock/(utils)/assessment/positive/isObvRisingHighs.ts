f8bd42c3b01064d54e32b61d0f1a85f5806158d3
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2024-09-20 17:50:09 +0800

    fixed bug

--- app/selectstock/(utils)/assessment/positive/isObvRisingHighs.ts
+++ app/selectstock/(utils)/assessment/positive/isObvRisingHighs.ts
@@ -7,19 +7,19 @@ export default function isObvRisingHighs(
   let length = datas.length - 1;
   const index1 = length - rollback_date;
 
-  const obvData = datas.map((data) => data.obv as number);
+  const obvData = datas.map((data) => data?.obv as number);
   const peak = findPeaksByGradient(obvData, 1);
   const peak1Index = peak[peak.length - 1];
   const peak2Index = peak[peak.length - 2];
   if (
-    datas[index1].obv !== undefined &&
-    datas[index1].obv !== null &&
-    datas[peak1Index].obv !== undefined &&
-    datas[peak1Index].obv !== null &&
-    datas[peak2Index].obv !== undefined &&
-    datas[peak2Index].obv !== null &&
-    datas[index1].obv > datas[peak2Index].obv &&
-    datas[peak1Index].obv > datas[peak2Index].obv
+    datas[index1]?.obv !== undefined &&
+    datas[index1]?.obv !== null &&
+    datas[peak1Index]?.obv !== undefined &&
+    datas[peak1Index]?.obv !== null &&
+    datas[peak2Index]?.obv !== undefined &&
+    datas[peak2Index]?.obv !== null &&
+    datas[index1]?.obv > datas[peak2Index]?.obv &&
+    datas[peak1Index]?.obv > datas[peak2Index].obv
   ) {
     return 'Obv頭頭高';
   }
